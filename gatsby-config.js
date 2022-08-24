1a380a6cc6afa3097eed0809d1bc65a944815343
jeffxjh <44387640+jeffxjh@users.noreply.github.com>
2022-08-24 14:57:31 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,15 @@ module.exports = {
     siteTitle: 'Running Page',
     siteUrl: 'https://yihong.run',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    description: '',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://running-page-jeffxjh.vercel.app/',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://github.com/jeffxjh/running_page/blob/master/README-CN.md',
       },
     ],
   },
